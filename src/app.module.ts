7db504ed218349869d0a4669c3eccf7c59f60904
Thenuwara <pramukhapoo@gmail.com>
2025-08-24 10:03:35 +0530

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 import { CashierModule } from './cashier/cashier.module';
 // import { StockModule } from './stock/stock.module';
+import { UsersModule } from './users/users.module';
 import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
@@ -14,6 +15,7 @@ import { SupplierModule } from './supplier/supplier.module';
     PrismaModule,
     AuthModule,
     CashierModule,
+    UsersModule,
     SupplierModule,
   ],
   controllers: [AppController],
