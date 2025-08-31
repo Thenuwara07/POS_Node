b64c4c27d3002f3e41eba53f31aa0bec9a6d097f
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-08-31 11:31:34 +0530

    create item dto final

--- src/stock/dto/create-item.dto.ts
+++ src/stock/dto/create-item.dto.ts
@@ -1,38 +1,43 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+  IsHexColor,
+} from 'class-validator';
 
-// import {
-//   IsInt,
-//   IsNotEmpty,
-//   IsOptional,
-//   IsString,
-//   Min,
-//   IsHexColor,
-// } from 'class-validator';
+export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
 
+  @IsString()
+  @IsNotEmpty()
+  barcode!: string;
 
+  @IsInt()
+  @Min(1)
+  categoryId!: number;
 
+  @IsInt()
+  @Min(1)
+  supplierId!: number;
 
+  @IsInt()
+  @IsOptional()
+  @Min(0)
+  reorderLevel?: number; // defaults to 0 in DB if omitted
 
-//   @IsInt()
-//   @Min(1)
-//   categoryId!: number;
+  @IsString()
+  @IsOptional()
+  gradient?: string | null; // HEX string or JSON per your comment
 
-//   @IsInt()
-//   @Min(1)
-//   supplierId!: number;
+  @IsString()
+  @IsOptional()
+  remark?: string | null;
 
-//   @IsInt()
-//   @IsOptional()
-//   @Min(0)
-//   reorderLevel?: number; // defaults to 0 in DB if omitted
-
-//   @IsString()
-//   @IsOptional()
-//   gradient?: string | null; // HEX string or JSON per your comment
-
-
-
-//   @IsHexColor()
-//   @IsOptional()
-//   colorCode?: string; // defaults to "#000000" in DB if omitted
-// }
-
+  @IsHexColor()
+  @IsOptional()
+  colorCode?: string; // defaults to "#000000" in DB if omitted
+}
