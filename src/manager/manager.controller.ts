da722ea0c727a647b75a39740b497737bfdcc296
Janeesha123 <janeeshachathurni2021@gmail.com>
2025-10-18 21:21:14 +0530

    manager folder

---
+++ src/manager/manager.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete, Query } from '@nestjs/common';
+import { ManagerService } from './manager.service';
+import { CreateManagerDto } from './dto/create-manager.dto';
+import { UpdateManagerDto } from './dto/update-manager.dto';
+
+@Controller('manager')
+export class ManagerController {
+  constructor(private readonly managerService: ManagerService) {}
+
+  @Post()
+  create(@Body() dto: CreateManagerDto) {
+    return this.managerService.create(dto);
+  }
+
+  @Get()
+  findAll(
+    @Query('search') search?: string,
+    @Query('role') role?: string,
+    @Query('limit') limit?: number,
+    @Query('offset') offset?: number,
+  ) {
+    return this.managerService.findAll(search, role, limit, offset);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.managerService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateManagerDto) {
+    return this.managerService.update(Number(id), dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.managerService.remove(Number(id));
+  }
+}
